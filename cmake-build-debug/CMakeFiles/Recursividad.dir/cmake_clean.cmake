722c11cc6c5e1495b6022d26c6d2230aeb976693
ArianaMendonza03 <mendozaariana954@gmail.com>
2021-07-21 20:47:35 +1200

    Actividad en clase

---
+++ cmake-build-debug/CMakeFiles/Recursividad.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Recursividad.dir/main.cpp.obj"
+  "Recursividad.exe"
+  "Recursividad.exe.manifest"
+  "Recursividad.pdb"
+  "libRecursividad.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Recursividad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Recursividad.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Recursividad.
+# This may be replaced when dependencies are built.
